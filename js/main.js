ab931ca6ba43e1e417de68d05851ed01d872c54b
Sergey <balakirev.sergey87@gmail.com>
2020-08-19 14:34:03 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// let Admin;
+// let Name = 'Jon';
+
+// Admin = Name;
+// alert(Admin);
+
+// const ourPlanetName = 'Земля';
+// let currentUserName;
