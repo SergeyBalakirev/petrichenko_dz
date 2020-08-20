d41b5fb2152ac59da9f66afae75c397efca0bec6
Sergey <balakirev.sergey87@gmail.com>
2020-08-20 13:31:09 +0300

    add all 1-st dz

--- js/main.js
+++ js/main.js
@@ -21,3 +21,37 @@
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+// Его вариант 
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+// Мой вариант 
+
+// const question = [];
+// question[0] = prompt('Один из последних просмотренных фильмов?', '');
+// question[1] = prompt('На сколько оцените его?', '');
+// question[2] = prompt('Один из последних просмотренных фильмов?', '');
+// question[3] = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[question[0]] = question[1];
+// personalMovieDB.movies[question[2]] = question[3];
+
+// console.log(personalMovieDB);
