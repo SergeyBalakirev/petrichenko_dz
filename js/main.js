d8178955a15e50b0fc742175d2091cb9337da9cc
Sergey <balakirev.sergey87@gmail.com>
2020-08-24 13:59:41 +0300

    add 2-nd dz

--- js/main.js
+++ js/main.js
@@ -119,3 +119,47 @@ for (let i = 1; i < 8; i++) {
   console.log(i);
 
 }
+
+
+// DZ - 2 
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', '');
+  const b = prompt('На сколько оцените его?', '');
+
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('Done!');
+
+  } else {
+    console.log('error');
+    i--;
+  }
+
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('киноман');
+} else {
+  console.log('ошибка');
+}
+
+
+console.log(personalMovieDB);
